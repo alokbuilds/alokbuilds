528ccb03557d5210707dda670e5a6c416cd3d960
Alok Kumar <alokbuilds.dev@gmail.com>
2026-06-03 22:28:55 +0530

    Fix allowed hosts

--- alokbuilds/settings/base.py
+++ alokbuilds/settings/base.py
@@ -194,14 +194,19 @@ USE_TZ = True
 # STATIC FILES
 # =========================================
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
 
-    BASE_DIR / 'static'
+    BASE_DIR / "static"
 
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 # =========================================
 # DEFAULT PRIMARY KEY
