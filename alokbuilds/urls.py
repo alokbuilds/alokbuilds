799d1ee5a021b0d612907d899047d9345ec8a2f4
Alok Kumar <alokbuilds.dev@gmail.com>
2026-08-14 18:18:03 +0530

    prodbuilds.jpg file updated

--- alokbuilds/urls.py
+++ alokbuilds/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path('', include('apps.core.urls')),
     path('learn/', include('apps.courses.urls')),
     path('dashboard/', include('apps.dashboard.urls')),
-    #path('payments/', include('apps.payments.urls')),
     path('portfolio/', include('apps.portfolio.urls')),
     path('projects/', include('apps.projects.urls')),
     path('resources/', include('apps.resources.urls')),
-    path('tools/', include('apps.tools.urls')),
+    #path('payments/', include('apps.payments.urls')),
+    #path('tools/', include('apps.tools.urls')),
 ]
